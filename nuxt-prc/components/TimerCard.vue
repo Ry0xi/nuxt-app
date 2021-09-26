e0387b4124b396be86d060571dcb92e85ccc8c32
Ry0xi <ryoki0525tnk@gmail.com>
2021-09-27 00:21:34 +0900

    一時停止や再開に対応。入力チェックなども追加。いったん完成。

--- nuxt-prc/components/TimerCard.vue
+++ nuxt-prc/components/TimerCard.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="timer-card">
-    <div class="remain-time">{{formatTime}}</div>
-    <input type="number" class="input-set-time no-spin" placeholder="3000 -> 30:00" :disabled="timerOn">
-    <div class="btn-start" v-if="!timerOn" @click="startTimer"></div>
-    <div class="btn-stop" v-if="timerOn" @click="stopTimer"></div>
-    <div class="status-lamp" :class="timerOn ? 'is-active' : ''"></div>
+  <div
+    class="timer-card"
+    :class="{isActive: timerOn, isPaused: timerPaused, isCompleted: timerCompleted}"
+  >
+    <div class="remain-time">{{formattedTime}}</div>
+    <TimerInput
+      class="input-set-time"
+      v-model="inputTimeStr"
+      :disabled="timerOn || timerPaused"
+    />
+    <div class="btn-operate-timer btn-start" v-if="!timerOn && !timerPaused && (min || sec)" @click="startTimer"></div>
+    <div class="btn-operate-timer btn-continue" v-if="!timerOn && timerPaused" @click="continueTimer"></div>
+    <div class="btn-operate-timer btn-pause" v-if="timerOn && !timerPaused && !timerCompleted" @click="pauseTimer"></div>
+    <div class="btn-operate-timer btn-reset" v-if="!timerOn && timerPaused || timerCompleted" @click="resetTimer"></div>
+    <div class="status-lamp"></div>
   </div>
 </template>
 
@@ -13,20 +22,39 @@ export default {
   name: "TimerCard",
   data() {
     return {
+      inputTime: '',
       min: 60,
       sec: 0,
       passedTime: 0,
       startedTime: null,
+      timerOn: false,
+      timerPaused: false,
+      timerCompleted: false,
       objTimer: null,
-      timerOn: false
+      minForCount: null,
+      secForCount: null
     }
   },
   methods: {
     startTimer() {
+      if (!this.min > 0 && !this.sec > 0) {
+        return false
+      }
       this.startedTime = Date.now()
+      this.minForCount = this.min
+      this.secForCount = this.sec
       this.timerOn = true
 
       // setInterval内でのthisはコンポーネントインスタンスを示さない
+      let self = this
+      this.objTimer = setInterval(function() {
+        self.count()
+      }
+      , 1000)
+    },
+    continueTimer() {
+      this.timerPaused = false
+      this.timerOn = true
       let self = this
       this.objTimer = setInterval(function() {
         self.count()
@@ -37,27 +65,45 @@ export default {
       clearInterval(this.objTimer)
       this.timerOn = false
     },
+    pauseTimer() {
+      this.stopTimer()
+      this.timerPaused = true
+    },
+    resetTimer() {
+      this.stopTimer()
+      this.timerPaused = false
+      this.timerCompleted = false
+    },
     count() {
       this.passedTime++
 
-      if (this.sec <= 0 && this.min >= 1) {
-        this.min--
-        this.sec = 59
-      } else if (this.sec <= 0 && this.min <= 0) {
+      if (this.secForCount <= 0 && this.minForCount >= 1) {
+        this.minForCount--
+        this.secForCount = 59
+      } else if (this.secForCount <= 0 && this.minForCount <= 0) {
         this.complete()
       } else {
-        this.sec--
+        this.secForCount--
       }
     },
     complete() {
       clearInterval(this.objTimer)
+      this.timerCompleted = true
     }
   },
   computed: {
-    formatTime() {
+    formattedTime() {
       let timeString = ""
-      let sMin = String(this.min)
-      let sSec = String(this.sec)
+      let sMin = null
+      let sSec = null
+      if (!this.timerOn && !this.timerPaused) {
+        sMin = String(this.min)
+        sSec = String(this.sec)
+      } else {
+        sMin = String(this.minForCount)
+        sSec = String(this.secForCount)
+      }
+
       if (sMin.length < 2) {
         sMin = "0" + sMin
       }
@@ -67,12 +113,32 @@ export default {
       timeString = sMin + " : " + sSec
       return timeString
     },
-    validateTime() {
-      if (this.min < 0) {
-        this.min = 0
-      }
-      if (this.min > 999) {
-        this.min = 999
+    inputTimeStr: {
+      get() {
+        return this.inputTime
+      },
+      set(newTime) {
+        this.inputTime = newTime
+        let min = Number(this.inputTime.slice(0, -2))
+        let sec = Number(this.inputTime.slice(-2))
+
+        if (min == NaN || min < 0) {
+          min = 0
+        }
+        if (min > 999) {
+          min = 999
+        }
+        if (sec == NaN || sec < 0) {
+          sec = 0
+        }
+        if (sec >= 60) {
+          sec %= 60
+          if (min <= 998) {
+            min += 1
+          }
+        }
+        this.min = min
+        this.sec = sec
       }
     }
   }
@@ -87,6 +153,7 @@ export default {
   height: 100px;
   border-radius: 16px;
   background-color: #1E1E1E;
+  box-shadow: 0 0 6px 2px #0e0e0e;
   font-size: 16px;
 }
 
@@ -97,12 +164,7 @@ export default {
     transform: translate(0, 0);
   }
 
-  .btn-start {
-    visibility: visible;
-    opacity: 1;
-  }
-
-  .btn-stop {
+  .btn-operate-timer {
     visibility: visible;
     opacity: 1;
   }
@@ -125,8 +187,7 @@ export default {
   transition: all 0.1s linear;
 }
 
-.btn-start,
-.btn-stop {
+.btn-operate-timer {
   width: 26px;
   height: 26px;
   border: 2px solid #ccc;
@@ -141,7 +202,8 @@ export default {
   transition: all 0.2s linear;
 }
 
-.btn-start {
+.btn-start,
+.btn-continue {
   &::after {
     content: "";
     display: block;
@@ -156,7 +218,7 @@ export default {
   }
 }
 
-.btn-stop {
+.btn-pause {
   &::after {
     content: "";
     display: block;
@@ -171,23 +233,33 @@ export default {
   }
 }
 
-.input-set-time {
-  display: block;
-  visibility: hidden;
-  opacity: 0;
-  position: absolute;
-  left: 10px;
-  bottom: 8px;
-  width: 7.4em;
-  height: 1.5em;
-  padding: 0 0.6em;
-  background-color: #aaa;
-  border-radius: 4px;
-  border: 1px solid #ddd;
-  transition: all 0.2s linear;
+.btn-reset {
+  top: 40%;
+  right: 8px;
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+  }
+  &::before {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    border: 2px solid #ccc;
+    border-left-color: transparent;
 
-  &:disabled {
-    opacity: 0.4 !important;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &::after {
+    width: 4.5px;
+    height: 4.5px;
+    clip-path: polygon(0 0, 0% 100%, 100% 100%);
+    background-color: #ccc;
+    top: 22%;
+    left: 24%;
   }
 }
 
@@ -201,17 +273,30 @@ export default {
   --status-color: red;
   background-color: var(--status-color, gray);
   box-shadow: 0 0 8px var(--status-color, gray);
-}
 
-.status-lamp.is-active {
-  --status-color: lightgreen;
-}
+  .isActive & {
+    --status-color: lightgreen;
+  }
+  .isPaused & {
+    --status-color: orange;
+  }
+  .isCompleted & {
+    --status-color: red;
+    animation: blinking-light 2.0s infinite;
+  }
 
+  @keyframes blinking-light {
+    0%, 100% {
+      opacity: 1;
+    }
 
-.no-spin::-webkit-inner-spin-button,
-.no-spin::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-    -moz-appearance:textfield;
+    50% {
+      opacity: 0.3;
+    }
+
+    60% {
+      opacity: 1;
+    }
+  }
 }
 </style>
---
+++ nuxt-prc/components/TimerInput.vue
@@ -0,0 +1,47 @@
+<!--
+  数字以外は返さないようにしているため、値は整数のみとなる
+-->
+<template>
+  <input
+    type="number"
+    class="no-spin"
+    placeholder="3000 -> 30:00"
+    :value="value"
+    @input="$emit('input', $event.target.value.replace(/[^0-9]/g, ''))"
+  >
+</template>
+
+<script>
+export default {
+  props: ['value']
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  display: block;
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  left: 10px;
+  bottom: 8px;
+  width: 7.4em;
+  height: 1.5em;
+  padding: 0 0.6em;
+  background-color: #aaa;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  transition: all 0.2s linear;
+
+  &:disabled {
+    opacity: 0.4 !important;
+  }
+}
+
+.no-spin::-webkit-inner-spin-button,
+.no-spin::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+    -moz-appearance:textfield;
+}
+</style>
